34be8e833a0ed7414c68fdcdd3e4b09619e8136e
zohre <zohrefarzam@gmail.com>
2020-04-06 11:00:05 -0700

    Updates

---
+++ precache-manifest.8e9572bdf596f2ba5c91e56509c2c511.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f195adb715852233fb49f79cdfaca733",
+    "url": "/panel/index.html"
+  },
+  {
+    "revision": "075c34284e924b05bd1b",
+    "url": "/panel/static/css/2.bfa6f0c0.chunk.css"
+  },
+  {
+    "revision": "39d6833dcbc9ab48951f",
+    "url": "/panel/static/css/main.6de13044.chunk.css"
+  },
+  {
+    "revision": "075c34284e924b05bd1b",
+    "url": "/panel/static/js/2.b52e04eb.chunk.js"
+  },
+  {
+    "revision": "83db559aaf2c6b344e8af045a8f58ac1",
+    "url": "/panel/static/js/2.b52e04eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d6833dcbc9ab48951f",
+    "url": "/panel/static/js/main.cc29b9b8.chunk.js"
+  },
+  {
+    "revision": "ddf4dafef6bec992bdeb",
+    "url": "/panel/static/js/runtime-main.144743a2.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/panel/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/panel/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
